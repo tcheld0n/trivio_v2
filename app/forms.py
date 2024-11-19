8384f4ca7c63bbe404ae9fc4e1d0c51c90c562e6
Willian Tcheldon <tcheldonwillian@gmail.com>
2024-11-18 22:10:20 -0300

    upd: + ajustes texto

--- app/forms.py
+++ app/forms.py
@@ -6,29 +6,29 @@ from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Lembrar-me')
+    submit = SubmitField('Entrar')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=(DataRequired(), Email()))
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Confirm Password', 
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=(DataRequired(), Email()))
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField('Confirme a Senha', 
                 validators=(DataRequired(), EqualTo('password')))
-    submit = SubmitField('Register')
+    submit = SubmitField('Registrar-se')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Username already exists')
+            raise ValidationError('Nome de usuário já existe.')
     
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Email already exists.')
+            raise ValidationError('O e-mail já está registrado.')
 
 class QuestionForm(FlaskForm):
-    options = RadioField('Options: ', validators=[DataRequired()], default=1)
-    submit = SubmitField('Next')
+    options = RadioField('Opções: ', validators=[DataRequired()], default=1)
+    submit = SubmitField('Próxima')
